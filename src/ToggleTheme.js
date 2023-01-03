b70048bd232b4e68affb7eb6416ce29e2c3f1ea7
SubhodipRoy161101 <subhodiproy161101@gmail.com>
2023-01-03 23:10:06 +0530

    Aligned Toggle button

--- src/ToggleTheme.js
+++ src/ToggleTheme.js
@@ -1,13 +1,35 @@
 import { React } from "react";
-import { Button, useColorMode } from "@chakra-ui/react";
+import {
+  Button,
+  Center,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 const ToggleTheme = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Button onClick={toggleColorMode}>
-      Toggle {colorMode === "light" ? "Dark" : "Light"}
-    </Button>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <Center>
+            <Button
+              onClick={toggleColorMode}
+              fontSize={20}
+              colorScheme="orange"
+              bg={useColorModeValue("orange.100", "orange.500")}
+              variant="ghost"
+              p={4}
+              mt={4}
+              borderRadius="xl"
+            >
+              Toggle {colorMode === "light" ? "Dark" : "Light"}
+            </Button>
+          </Center>
+        </div>
+      </div>
+    </div>
   );
 };
 
